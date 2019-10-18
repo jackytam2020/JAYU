614b20217c7902b2796451070163d4ea5f032b89
jackytam <jackytam2020@gmail.com>
2019-10-18 01:08:25 -0700

    Added Calendar

---
+++ calendar/App.js
@@ -0,0 +1,21 @@
+/**
+ * Sample React Native App
+ * https://github.com/facebook/react-native
+ *
+ * @format
+ * @flow
+ */
+
+import React from 'react';
+import {View, Text} from 'react-native';
+import Schedule from './comps/calendar'
+
+function App(){
+  return(
+    <View>
+      <Schedule />
+    </View>
+  )
+}
+
+export default App;
