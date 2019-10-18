6d008deb0d6e27d93d92f95617c877af35cad049
jackytam <jackytam2020@gmail.com>
2019-10-18 00:13:02 -0700

    Added homescreen

---
+++ homescreen/styles/homescreenStyle.js
@@ -0,0 +1,35 @@
+import {StyleSheet} from 'react-native';
+
+
+
+const styles = StyleSheet.create({ 
+    home: {
+        display: 'flex',
+        width:'100%',
+        height:'100%',
+        position: 'absolute',
+        alignItems:'center'
+        
+    },
+    container: {
+        position: 'relative',
+        width:'85%',
+        height:'80%',
+        top:'12%',
+        display:'flex'
+    },
+    top: {
+        position: 'relative',
+        width:'100%',
+        height:'40%',
+    },
+    bottom: {
+        position: 'relative',
+        width:'100%',
+        height:'55%',
+        marginTop:'18%',
+
+    }    
+  })
+  
+  export default styles;
